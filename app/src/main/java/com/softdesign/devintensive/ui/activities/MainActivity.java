7bbec61ec0938ca2c9ea5b84eddcb924a5b28044
YuriBtr <yuribtr@gmail.com>
2016-06-29 03:42:39 +0300

    interface made by specification  rounded avatar made  added images  added save/load of user data  processing Back button correctly

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,22 +1,53 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.os.PersistableBundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import java.util.*;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.data.managers.DataManager;
 import com.softdesign.devintensive.utils.ConstantManager;
+import com.softdesign.devintensive.utils.RoundedAvatarDrawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    private static final String TAG= ConstantManager.TAG_PREFIX+"Main Activity";
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    private DataManager mDataManager;
+    private int mCurrentEditMode = 0;
+
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout drawer_l;
+    private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private ImageView avatar_iv;
+    private NavigationView navigation_v;
+    private EditText mUserPhone, mUserMail, mUserVK, mUserGit, mUserBio;
+
+    private List<EditText> mUserInfoViews;
 
 
     @Override
@@ -25,12 +56,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
 
+        mDataManager=new DataManager().getInstance();
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mUserPhone = (EditText) findViewById(R.id.phone_at);
+        mUserMail = (EditText) findViewById(R.id.email_et);
+        mUserVK = (EditText) findViewById(R.id.vk_et);
+        mUserGit = (EditText) findViewById(R.id.repository_et);
+        mUserBio = (EditText) findViewById(R.id.about_et);
+
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserMail);
+        mUserInfoViews.add(mUserVK);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUserBio);
+
+//        mCallImg.setOnClickListener(this);
+        mFab.setOnClickListener(this);
+        setupToolbar();
+        setupDrawer();
+        loadUserInfoValue();
+
+        drawer_l = (DrawerLayout) findViewById(R.id.navigation_drawer);
+
+        navigation_v = (NavigationView) findViewById(R.id.navigation_view);
+        avatar_iv = (ImageView) navigation_v.getHeaderView(0).findViewById(R.id.avatar);
+        avatar_iv.setImageBitmap(RoundedAvatarDrawable.getRoundedBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.avatar)));
 
         if (savedInstanceState == null) {
             //активити запускается впервые
+//            showSnackbar("активити запускается впервые");
+//            showToast("активити запускается впервые");
         } else {
+            mCurrentEditMode= savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY,0);
+            changeEditMode(mCurrentEditMode);
             //активити уже создавалось
+//            showSnackbar("активити уже создавалось");
+//            showToast("активити уже создавалось");
         }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -69,11 +146,120 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onRestart");
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+//                showSnackbar("click");
+                if (mCurrentEditMode == 0) {
+                    changeEditMode(1);
+                    mCurrentEditMode = 1;
+                } else {
+                    changeEditMode(0);
+                    mCurrentEditMode = 0;
+                }
+                break;
+//            case R.id.call_img:
+//                showProgress();
+//                runWithDelay();
+//                break;
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && (event.getRepeatCount() == 0)) {
+            if(drawer_l.isDrawerOpen(GravityCompat.START)) {
+                drawer_l.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState");
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
 
     }
 
+    private void showSnackbar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                                                             @Override
+                                                             public boolean onNavigationItemSelected(MenuItem item) {
+                                                                 showSnackbar(item.getTitle().toString());
+                                                                 item.setChecked(true);
+                                                                 mNavigationDrawer.closeDrawer(GravityCompat.START);
+                                                                 return false;
+                                                             }
+                                                         }
+        );
+
+    }
+
+    private void changeEditMode(int mode) {
+
+        if (mode == 1) {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        }
+        else {
+            mFab.setImageResource(R.drawable.ic_create_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+                saveUserInfoValue();
+            }
+        }
+    }
+
+    private void loadUserInfoValue() {
+        List<String> userData = mDataManager.getPreferencesManager().loadUserProfileData();
+        for (int i=0;i<userData.size();i++){
+            mUserInfoViews.get(i).setText(userData.get(i));
+
+        }
+    };
+
+    private void saveUserInfoValue() {
+        List<String> userData = new ArrayList<>();
+        for (EditText userFieldView : mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferencesManager().saveUserProfileData(userData);
+    };
+
+//    protected void runWithDelay (){
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                //TODO:выполнить с задержкой
+//                hideProgress();
+//            }
+//        },5000);
+//    }
 }
