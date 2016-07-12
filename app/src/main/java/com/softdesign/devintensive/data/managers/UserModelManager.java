ab12d3c32f8a51aa4f935693685d772de6980124
YuriBtr <yuribtr@gmail.com>
2016-07-12 17:05:58 +0300

    1. Added network login + loading data via Retrofit (w/o upload yet) 2. Added models, interceptors, REST service, converters 3. Made dynamic height for block of counters via Behaviors 4. Fixed keyboard behavior of Login window

--- app/src/main/java/com/softdesign/devintensive/data/managers/UserModelManager.java
+++ app/src/main/java/com/softdesign/devintensive/data/managers/UserModelManager.java
@@ -10,14 +10,12 @@ public class UserModelManager {
     public static void saveUserModelToPreferenses(DataManager dataManager, UserModelRes userModel) {
         dataManager.getPreferencesManager().setAuthToken(userModel.getData().getToken());
         dataManager.getPreferencesManager().saveUserId(userModel.getData().getUser().getId());
-
         int[] userRatings = {
                 userModel.getData().getUser().getProfileValues().getRating(),
                 userModel.getData().getUser().getProfileValues().getLinesCode(),
                 userModel.getData().getUser().getProfileValues().getProjects()
         };
         dataManager.getPreferencesManager().saveUserProfileValues(userRatings);
-
         List<String> userFields = new ArrayList<>();
         userFields.add(userModel.getData().getUser().getContacts().getPhone());
         userFields.add(userModel.getData().getUser().getContacts().getEmail());
@@ -28,15 +26,10 @@ public class UserModelManager {
             userFields.add("");
         }
         userFields.add(userModel.getData().getUser().getPublicInfo().getBio());
-
         dataManager.getPreferencesManager().saveUserFio(userModel.getData().getUser().getFirstName()+" "+userModel.getData().getUser().getSecondName());
-
         dataManager.getPreferencesManager().saveUserProfileData(userFields);
-
         Uri uri = Uri.parse(userModel.getData().getUser().getPublicInfo().getPhoto());
         dataManager.getPreferencesManager().saveUserPhoto(uri);
-
-
         uri = Uri.parse(userModel.getData().getUser().getPublicInfo().getAvatar());
         dataManager.getPreferencesManager().saveUserAvatar(uri);
 
